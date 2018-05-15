a630e1e8a101fcc8d32f4cd5b6d28bb355e911e7
DESKTOP-GDM5KI7\Mreow <Sevarish@hotmail.com>
2018-05-15 23:26:40 +0200

    no message

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    Transform myT;
+    public int speedUp = 4;
+    public int speedHor = 7;
+    int maxHorRight = 9;
+    int maxHorLeft = -9;
+	// Use this for initialization
+	void Start () {
+        myT = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxisRaw("Horizontal");
+        
+        if (myT.position.x <= maxHorRight && x < 0)
+        {
+            transform.Translate(x * Time.deltaTime * -speedHor, 0, 0);
+        }
+
+        if (myT.position.x >= maxHorLeft && x > 0)
+        {
+            transform.Translate(x * Time.deltaTime * -speedHor, 0, 0);
+        }
+
+
+
+        transform.Translate(0, speedUp * Time.deltaTime, 0);
+    }
+}
